134a964f9d3dbb016ffda940579fcf4873aef7ef
Popov-555 <78635331+Popov-555@users.noreply.github.com>
2021-02-27 10:13:11 +0300

    Add files via upload

--- Лаб раб 2 Попов ИЛья/Program.cs
+++ Лаб раб 2 Попов ИЛья/Program.cs
@@ -2,54 +2,140 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
- 
-namespace oap { }
 
-public class Program
+namespace oap
+
+
 {
-    static void Main(string[] args)
+    // про классы мы пока не говорили...
+    class Program
     {
-        
-        Console.WriteLine("Введите три числа ");
-
-        //возведение в степень: Math.pow(double, double)
-        var a = Convert.ToDouble(Console.ReadLine());
-        var b = Convert.ToDouble(Console.ReadLine());
-        var c = Convert.ToDouble(Console.ReadLine());
-
-        if(a>=0) a = Math.Pow(a, 2);
-        else a = Math.Pow(b, 4);
-
-        b = b >= 0 ? Math.Pow(b, 2) : Math.Pow(b, 4);
-
-        c = c >= 0 ? Math.Pow(c, 2) : Math.Pow(c, 4);
-        Console.WriteLine($"a={a} b={b} c={c}");
-
-        Console.ReadLine();
-        
-
-Console.WriteLine("Ввод Х1:");
-var X1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Ввод Y1:");
-var Y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Ввод X2:");
-var X2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Ввод Y2:");
-var Y2 = Convert.ToDouble(Console.ReadLine());
-var Dlina1 = Math.Sqrt(Math.Pow(X1, 2) + Math.Pow(Y1, 2));
-var Dlina2 = Math.Sqrt(Math.Pow(X2, 2) + Math.Pow(Y2, 2));
-if (Dlina1 < Dlina2)
-{
-    Console.WriteLine("первая точка ближе");
-}
-else
+        // точка входа в программу
+        static void Main(string[] args)
         {
-    Console.WriteLine("вторая точка ближе");
-   Console.ReadLine();
+            var MassivDate = new List<DateTime>() {
+new DateTime(2021, 1, 15),
+new DateTime(2021, 2, 20),
+new DateTime(2011, 5, 2),
+new DateTime(2015, 5, 1),
+new DateTime(2013, 5, 3),
+new DateTime(2020, 1, 17)};
+            var result = new Dictionary<int, int>();
+            foreach (DateTime TekushayaData in MassivDate)
+            {
+                if (result.ContainsKey(TekushayaData.Month))
+                {
+                    result[TekushayaData.Month] = result[TekushayaData.Month] + 1;
+                }
+                else
+                    result[TekushayaData.Month] = 1;
+            }
+            var PopularMonth = -1;
+            var MaxCount = 0;
+            foreach (KeyValuePair<int, int> keyValue in result)
+            {
+                if (keyValue.Value > MaxCount)
+                {
+                    MaxCount = keyValue.Value;
+                    PopularMonth = keyValue.Key;
+                }
+            }
+            Console.WriteLine($"Самый популярный месяц {PopularMonth}");
+            Console.Write("Press ENTER to continue...");
+            Console.ReadLine();
         }
     }
 }
+
+
+
+/*
+Console.WriteLine("Input posledovatelnost: ");
+string Posledovatelnost = Console.ReadLine();
+var StrelkaCoint = 0;
+var strelka1 = ">>-->";
+var strelka2 = "<--<<";
+int poz = 0;
+while((poz = Posledovatelnost.IndexOf(strelka1, poz))>=0)
+{
+    StrelkaCoint++;
+    poz++;
+}
+poz = 0;
+while ((poz = Posledovatelnost.IndexOf(strelka2, poz)) >= 0)
+{
+    StrelkaCoint++;
+    poz++;
+}
+Console.WriteLine($"Количество стрелок: {StrelkaCoint}");
+*/
+/*
+DateTime date1=new DateTime(2021,02,21);
+DateTime date2=new DateTime(2026,02,25);
+Console.WriteLine(Math.Abs(date1.Subtract(date2).TotalDays));
+*/
+/*
+Console.WriteLine("Введите год: ");
+DateTime date1 = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+date1 = date1.AddDays(255);
+var DateString = date1.ToString("dd MMMM");
+Console.WriteLine($"день программиста отмечается  {DateString}");
+*/
+/*
+Console.WriteLine("Введите день: ");
+DateTime date1 = new DateTime(2021, 1, 1);
+date1 = date1.AddDays(Convert.ToInt32(Console.ReadLine())-1);
+var DateString = date1.ToString("dddd");
+var voskresenie = date1.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)date1.DayOfWeek;
+Console.WriteLine($"день недели {DateString}, и номер дня недели {voskresenie}");
+*/
+
+/*
+ DateTime date1 = new DateTime(2021, 06, 21);
+Console.WriteLine("Введите дату в формате дд.мм.гггг: ");
+string date2 = Console.ReadLine();
+string[] StringArray = date2.Split('.');
+var Date3 = new DateTime(Convert.ToInt32(StringArray[2]), Convert.ToInt32(StringArray[1]), Convert.ToInt32(StringArray[0]));
+var Raznitca = date1.Subtract(Date3);
+if (Raznitca.TotalDays == 0)
+{
+    Console.WriteLine("Сегодня экзамен");
+}
+else if (Raznitca.TotalDays > 0)
+    Console.WriteLine($"Экзамен будет через {Raznitca.TotalDays} дней");
+else
+    Console.WriteLine($"Экзамен был {Math.Abs(Raznitca.TotalDays)} дней назад");
+
+
+Console.Write("Press ENTER to continue...");
+Console.ReadLine();
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
