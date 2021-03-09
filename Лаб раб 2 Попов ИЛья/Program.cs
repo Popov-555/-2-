cafb2c9d68a5fa03b748bef50e0c7bcfc388fd96
попов илья <veronichka.toporova@list.ru>
2021-03-09 16:36:51 +0300

    laba 2

--- Лаб раб 2 Попов ИЛья/Program.cs
+++ Лаб раб 2 Попов ИЛья/Program.cs
@@ -1,335 +1,302 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace oap
 {
-
     class Program
     {
-        // точка входа в программу
+
+        static void Fun1()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем диска: {drive.TotalSize}");
+                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            // Fun1();
+            //Fun2();
+            // Fun3();
+            //Fun4();
+            //Fun5();
+            //Fun6();
+            //Fun7();
+            //Fun8();
+            // Fun9();
+            //Fun10();
+            //Fun11();
+            //Fun12();
+            //Fun13();
+            //Fun14();
+            //Fun15();
+            //Fun16();
+            //Fun17();
+            //Fun18();
+            //Fun19();
+
+            Console.ReadLine();
+        }
 
 
-            //ExceptionTest();
-            //ExceptionTest2();
-            //ExceptionTest3();
-            //ExceptionTest4();
-            //ExceptionTest5();
-            //ExceptionTest6();
-            //ExceptionTest7();
-            //ExceptionTest8();
-            //ExceptionTest9();
-            //ExceptionTest10();
-            //ExceptionTest11();
-            //ExceptionTest12();
-            //ExceptionTest13();
-            //ExceptionTest14();
-            //ExceptionTest15();
-            //ExceptionTest16();
-            //ExceptionTest17();
-            //ExceptionTest18();
-            ExceptionTest19();
-            Console.ReadKey();
-        }
-        static void ExceptionTest()
+
+        static void Fun3()
         {
-            int x = 5;
-            int y = x / 0;
-            Console.WriteLine($"Результат: {y}");
-            Console.WriteLine("Конец программы");
-            Console.Read();
+            string path = @"C:\i-31\popov";
+            string subpath = "popov";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
         }
-        static void ExceptionTest2()
+
+        static void Fun4()
         {
+            string dirName = "C:\\Program Files";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+
+            Console.WriteLine($"Название каталога: {dirInfo.Name}");
+            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+        }
+        static void Fun2()
+        {
+            string dirName = "C:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+        static void Fun5()
+        {
+            string dirName = @"C:\i31";
+
+            Directory.Delete(dirName, true);
+
+        }
+        static void Fun6()
+        {
+            string oldPath = @"C:\SomeFolder";
+            string newPath = @"C:\SomeDir";
+            DirectoryInfo dirInfo = new DirectoryInfo(oldPath);
+            if (dirInfo.Exists && Directory.Exists(newPath) == false)
+            {
+                dirInfo.MoveTo(newPath);
+            }
+
+        }
+        static void Fun7()
+        {
+            string path = @"C:\apache\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("Имя файла: {0}", fileInf.Name);
+                Console.WriteLine("Время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf.Length);
+            }
+        }
+
+        static void Fun8()
+        {
+            string path = @"C:\apache\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+                // альтернатива с помощью класса File
+                // File.Delete(path);}
+            }
+        }
+        static void Fun9()
+        {
+            string path = @"C:\apache\hta.txt";
+            string newPath = @"C:\SomeDir\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(newPath);
+                // альтернатива с помощью класса File
+                // File.Move(path, newPath);
+            }
+        }
+
+        static void Fun10()
+        {
+            string path = @"C:\apache\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+                // альтернатива с помощью класса File
+                // File.Delete(path);
+            }
+        }
+
+        static void Fun11()
+        {
+            string path = @"C:\apache\hta.txt";
+            string newPath = @"C:\SomeDir\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(newPath);
+                // альтернатива с помощью класса File
+                // File.Move(path, newPath);
+            }
+        }
+        static void Fun12()
+        {
+            string path = @"C:\apache\hta.txt";
+            string newPath = @"C:\SomeDir\hta.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.CopyTo(newPath, true);
+                // альтернатива с помощью класса File
+                // File.Copy(path, newPath, true);
+            }
+        }
+        static void Fun13()
+
+        {
+            
+                // создаем каталог для файла
+                string path = @"C:\SomeDir2";
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+                Console.WriteLine("Введите строку для записи в файл:");
+                string text = Console.ReadLine();
+
+                // запись в файл
+                using (FileStream fstream = new FileStream($"{path}\note.txt", FileMode.OpenOrCreate))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    // запись массива байтов в файл
+                    fstream.Write(array, 0, array.Length);
+                    Console.WriteLine("Текст записан в файл");
+                }
+
+                // чтение из файла
+                using (FileStream fstream = File.OpenRead($"{path}\note.txt"))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = new byte[fstream.Length];
+                    // считываем данные
+                    fstream.Read(array, 0, array.Length);
+                    // декодируем байты в строку
+                    string textFromFile = System.Text.Encoding.Default.GetString(array);
+                    Console.WriteLine($"Текст из файла: {textFromFile}");
+                }
+
+                Console.ReadLine();
+
+            
+
+
+        }
+        static void Fun14()
+        {
+
+            FileStream fstream = null;
             try
             {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
+                fstream = new FileStream(@"D:\note3.dat", FileMode.OpenOrCreate);
+                // операции с потоком
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("Возникло исключение!");
+
             }
             finally
             {
-                Console.WriteLine("Блок finally");
+                if (fstream != null)
+                    fstream.Close();
             }
-            Console.WriteLine("Конец программы");
-            Console.Read();
         }
-        static void ExceptionTest3()
+
+        static void Fun15()
         {
+
+            string writePath = @"C:\SomeDir\hta.txt";
+
+            string text = "Привет мир!\nПока мир...";
             try
             {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
-            }
-            catch
-            {
-                Console.WriteLine("Возникло исключение!");
-            }
-        }
-        static void ExceptionTest4()
-        {
-            Console.WriteLine("Введите число");
-            int x = Int32.Parse(Console.ReadLine());
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                }
 
-            x *= x;
-            Console.WriteLine("Квадрат числа: " + x);
-            Console.Read();
-        }
-        static void ExceptionTest5()
-        {
-            Console.WriteLine("Введите число");
-            int x;
-            string input = Console.ReadLine();
-            if (Int32.TryParse(input, out x))
-            {
-                x *= x;
-                Console.WriteLine("Квадрат числа: " + x);
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("Дозапись");
+                    sw.Write(4.5);
+                }
+                Console.WriteLine("Запись выполнена");
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Некорректный ввод");
-            }
-            Console.Read();
-        }
-        static void ExceptionTest6()
-        {
-            try
-            {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Возникло исключение DivideByZeroException");
+                Console.WriteLine(e.Message);
             }
         }
-        static void ExceptionTest7()
+        static void Fun16() { }
+        [Serializable]
+        class Person
         {
-            try
-            {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine($"Возникло исключение {ex.Message}");
-            }
-        }
-        static void ExceptionTest8()
-        {
-            int x = 1;
-            int y = 0;
+            public string Name { get; set; }
+            public int Year { get; set; }
 
-            try
+            public Person(string name, int year)
             {
-                int result = x / y;
+                Name = name;
+                Year = year;
             }
-            catch (DivideByZeroException) when (y == 0 && x == 0)
-            {
-                Console.WriteLine("y не должен быть равен 0");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void ExceptionTest9()
-        {
-            try
-            {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Исключение: {ex.Message}");
-                Console.WriteLine($"Метод: {ex.TargetSite}");
-                Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
-            }
-
-            Console.Read();
-        }
-        static void ExceptionTest10()
-        {
-            try
-            {
-                int[] numbers = new int[4];
-                numbers[7] = 9;     // IndexOutOfRangeException
-
-                int x = 5;
-                int y = x / 0;  // DivideByZeroException
-                Console.WriteLine($"Результат: {y}");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Возникло исключение DivideByZeroException");
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.Read();
-        }
-        static void ExceptionTest11()
-        {
-            try
-            {
-                object obj = "you";
-                int num = (int)obj;     // InvalidCastException
-                Console.WriteLine($"Результат: {num}");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Возникло исключение DivideByZeroException");
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Возникло исключение IndexOutOfRangeException");
-            }
-
-            Console.Read();
-        }
-        static void ExceptionTest12()
-        {
-            try
-            {
-                object obj = "you";
-                int num = (int)obj;     // InvalidCastException
-                Console.WriteLine($"Результат: {num}");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Возникло исключение DivideByZeroException");
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Возникло исключение IndexOutOfRangeException");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Исключение: {ex.Message}");
-            }
-            Console.Read();
-
-        }
-        static void ExceptionTest13()
-        {
-            int? a = 42;
-            if (a is int valueOfA)
-            {
-                Console.WriteLine($"a is {valueOfA}");
-            }
-            else
-            {
-                Console.WriteLine("a does not have a value");
-            }
-        }
-        static void ExceptionTest14()
-        {
-            int? b = null;
-            if (b.HasValue)
-            {
-                Console.WriteLine($"b is {b.Value}");
-            }
-            else
-            {
-                Console.WriteLine("b does not have a value");
-            }
-        }
-        static void ExceptionTest15()
-        {
-            int? c = 7;
-            if (c != null)
-            {
-                Console.WriteLine($"c is {c.Value}");
-            }
-            else
-            {
-                Console.WriteLine("c does not have a value");
-            }
+        
         }
 
-        static void ExceptionTest16()
-        {
-            int? a = 28;
-            int b = a ?? -1;
-            Console.WriteLine($"b is {b}");  // output: b is 28
 
-            int? c = null;
-            int d = c ?? -1;
-            Console.WriteLine($"d is {d}");  // output: d is -1
-        }
-        static void ExceptionTest17()
-        {
-            int? a = 10;
-            Console.WriteLine($"{a} >= null is {a >= null}");
-            Console.WriteLine($"{a} < null is {a < null}");
-            Console.WriteLine($"{a} == null is {a == null}");
-            // Output:
-            // 10 >= null is False
-            // 10 < null is False
-            // 10 == null is False
 
-            int? b = null;
-            int? c = null;
-            Console.WriteLine($"null >= null is {b >= c}");
-            Console.WriteLine($"null == null is {b == c}");
-            // Output:
-            // null >= null is False
-            // null == null is True
-        }
-        static void ExceptionTest18()
-        {
-            int a = 41;
-            object aBoxed = a;
-            int? aNullable = (int?)aBoxed;
-            Console.WriteLine($"Value of aNullable: {aNullable}");
-
-            object aNullableBoxed = aNullable;
-            if (aNullableBoxed is int valueOfA)
-            {
-                Console.WriteLine($"aNullableBoxed is boxed int: {valueOfA}");
-            }
-            // Output:
-            // Value of aNullable: 41
-            // aNullableBoxed is boxed int: 41
-        }
-        static void ExceptionTest19()
-        {
-            object z = 200;
-            object t = z ?? 44;
-            Console.WriteLine(t);
-        }
     }
 }
 
 
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
 
 
 
